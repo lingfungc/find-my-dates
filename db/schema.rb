f4988bf2d5e973ad194c01392dc02627ae228e8c
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-06 12:24:18 +0000

    updated users schema with username first-name last-name

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_115715) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
