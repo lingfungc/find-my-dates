5b051f4c491a8ec99b63cbd05ff701eca75b1772
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-07 00:16:22 +0000

    added follower-following relationship to user-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,14 @@ class User < ApplicationRecord
 
   has_many_attached :photos
 
-  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
-  has_many :followers, through: :follower_relationships, source: :follower
+  has_one :follower_relationship, foreign_key: :following_id, class_name: 'Follow'
+  has_one :follower, through: :follower_relationship, source: :follower
 
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
-  has_many :following, through: :following_relationships, source: :following
+  has_many :followings, through: :following_relationships, source: :followings
 
   def follow(user_id)
+    # raise
     following_relationships.create(following_id: user_id)
   end
 
